1104bf9fcef04ee712a1caccd1b331bd676a5b45
Minkyu Kim <58907624+minkyu-types@users.noreply.github.com>
2025-02-24 23:04:27 +0900

    refactor: 공용 컴포넌트 fontWeight 추가
    - TextField 위에 위치하는 Text 컴포넌트에 FontWeight.SemiBold 적용

--- presentation/src/main/java/com/and/presentation/component/HintErrorTextField.kt
+++ presentation/src/main/java/com/and/presentation/component/HintErrorTextField.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.and.presentation.ui.Primary0
@@ -38,6 +39,7 @@ fun HintErrorTextField(
         Text(
             text = valueTitle,
             fontSize = 14.sp,
+            fontWeight = FontWeight.SemiBold,
             modifier = Modifier.padding(start = 6.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
