5bde84422ec4c18648208e256e9e35d732b3599c
Minkyu Kim <58907624+minkyu-types@users.noreply.github.com>
2025-02-25 18:36:09 +0900

    design: Font, Typopgraphy 파일 추가
    - SpoqaHanSansNeo 및 폰트 옵션 설정
    - 기존 styles.xml 파일 삭제

---
+++ presentation/src/main/java/com/and/presentation/ui/Typography.kt
@@ -0,0 +1,96 @@
+package com.and.presentation.ui
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.em
+
+val Display1 = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 56.sp,
+    lineHeight = 64.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Display2 = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 40.sp,
+    lineHeight = 48.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Title1 = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 36.sp,
+    lineHeight = 44.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Title2 = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 28.sp,
+    lineHeight = 36.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Title3 = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 24.sp,
+    lineHeight = 32.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Heading1 = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 22.sp,
+    lineHeight = 30.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Heading2 = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 20.sp,
+    lineHeight = 28.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Body1Normal = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 16.sp,
+    lineHeight = 22.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Body1Reading = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Body2Normal = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Body2Reading = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 14.sp,
+    lineHeight = 22.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Label1 = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 12.sp,
+    lineHeight = 18.sp,
+    letterSpacing = (-0.02).em
+)
+
+val Caption = TextStyle(
+    fontFamily = SpoqaHanSansNeo,
+    fontSize = 11.sp,
+    lineHeight = 16.sp,
+    letterSpacing = (-0.02).em
+)
