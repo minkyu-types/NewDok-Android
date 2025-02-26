ec2100ff5a59ae9cd55d521c2283eaba09e51d08
loki <lion9638@naver.com>
2025-02-27 01:51:57 +0900

    refactor: RegisterStep route명 변경

--- presentation/src/main/java/com/and/presentation/screen/register/RegisterStep.kt
+++ presentation/src/main/java/com/and/presentation/screen/register/RegisterStep.kt
@@ -1,13 +1,13 @@
 package com.and.presentation.screen.register
 
 enum class RegisterStep(val route: String, val step: Int) {
-    STEP_1_AUTH("step1_auth", 1),
-    STEP_2_ID("step2_id", 2),
-    STEP_3_PASSWORD("step2_password", 3),
-    STEP_4_USER_INFO("step3_user_info", 4),
-    STEP_5_TERMS("step4_terms", 5),
-    STEP_6_COMPLETE("step5_complete", 6),
-    STEP_7_ADDITIONAL_INFO("step6_additional_info", 7);
+    STEP_1_AUTH("register_step1_auth", 1),
+    STEP_2_ID("register_step2_id", 2),
+    STEP_3_PASSWORD("register_step2_password", 3),
+    STEP_4_USER_INFO("register_step3_user_info", 4),
+    STEP_5_TERMS("register_step4_terms", 5),
+    STEP_6_COMPLETE("register_step5_complete", 6),
+    STEP_7_ADDITIONAL_INFO("register_step6_additional_info", 7);
 
     companion object {
         fun getStepByRoute(route: String): Int {
