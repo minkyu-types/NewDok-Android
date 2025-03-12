4641689d6ef9c30ac88747c934f6b9ae01998bbd
loki <lion9638@naver.com>
2025-03-12 17:46:10 +0900

    feat: 관심사 Grid 리스트 컴포넌트 분리

--- presentation/src/main/java/com/and/presentation/screen/preinvestigation/InvestigationStep2Screen.kt
+++ presentation/src/main/java/com/and/presentation/screen/preinvestigation/InvestigationStep2Screen.kt
@@ -1,10 +1,7 @@
 package com.and.presentation.screen.preinvestigation
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,13 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -30,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.and.domain.model.type.InterestCategory
 import com.and.presentation.R
+import com.and.presentation.component.InvestigationInterestList
 import com.and.presentation.component.button.ConditionalNextButton
 import com.and.presentation.ui.Body2Normal
 import com.and.presentation.ui.Caption_Heavy
@@ -47,7 +41,6 @@ fun InvestigationStep2Screen(
     modifier: Modifier = Modifier
 ) {
     var selectedInterests = rememberSaveable { mutableStateOf(emptySet<InterestCategory>()) }
-    val categories = InterestCategory.entries
 
     Column(
         modifier = Modifier
@@ -77,7 +70,6 @@ fun InvestigationStep2Screen(
             )
             Spacer(modifier = Modifier.height(40.dp))
             InvestigationInterestList(
-                categories = categories,
                 selectedInterests = selectedInterests.value,
                 onInterestClick = { interest ->
                     val updated = selectedInterests.value.toMutableSet()
@@ -95,36 +87,6 @@ fun InvestigationStep2Screen(
             onClick = { onNext() },
             modifier = Modifier.padding(24.dp),
         )
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun InvestigationInterestList(
-    categories: List<InterestCategory>,
-    selectedInterests: Set<InterestCategory>,
-    onInterestClick: (InterestCategory) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    CompositionLocalProvider(
-        LocalOverscrollConfiguration provides null
-    ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier.padding(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(
-                items = categories,
-            ) { interest ->
-                InvestigationInterestItem(
-                    interest = interest,
-                    isSelected = interest in selectedInterests,
-                    onClick = onInterestClick
-                )
-            }
-        }
     }
 }
 
