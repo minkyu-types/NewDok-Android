292782da6181a9d56357c7b79286560850537cd4
loki <lion9638@naver.com>
2025-03-12 17:47:11 +0900

    chore: 뉴스레터 상세 페이지 네이밍 변경

--- presentation/src/main/java/com/and/presentation/screen/newsletterdetail/NewsLetterDetailScreen.kt
+++ presentation/src/main/java/com/and/presentation/screen/newsletterdetail/NewsLetterDetailScreen.kt
@@ -1,4 +1,4 @@
-package com.and.presentation.screen.newsletter
+package com.and.presentation.screen.newsletterdetail
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
