4da593c43a72b9559142fdf9b47570b42d3edf5f
loki <lion9638@naver.com>
2025-03-14 18:15:11 +0900

    feat: 서비스 피드백 화면 구현

---
+++ presentation/src/main/java/com/and/presentation/screen/mypage/ServiceFeedbackScreen.kt
@@ -0,0 +1,84 @@
+package com.and.presentation.screen.mypage
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.and.presentation.R
+import com.and.presentation.component.topbar.TopBar
+import com.and.presentation.ui.Body2Normal
+import com.and.presentation.ui.Caption_Heavy
+import com.and.presentation.ui.Caption_Neutral
+import com.and.presentation.ui.DefaultWhiteTheme
+import com.and.presentation.ui.Heading2
+
+@Composable
+fun ServiceFeedbackScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        TopBar(
+            title = stringResource(R.string.service_feedback),
+            onNavigationIconClick = onBack,
+        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.weight(1f)
+                .padding(horizontal = 20.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.service_feedback_title),
+                style = Heading2,
+                fontWeight = FontWeight.Bold,
+                color = Caption_Heavy,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = stringResource(R.string.service_feedback_body),
+                style = Body2Normal,
+                fontWeight = FontWeight.Medium,
+                color = Caption_Neutral,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview(
+    name = "ServiceFeedbackScreen Preview",
+    showBackground = true,
+    showSystemUi = false
+)
+@Composable
+private fun ServiceFeedbackScreenPreview() {
+    DefaultWhiteTheme {
+        ServiceFeedbackScreen(
+            onBack = {
+
+            }
+        )
+    }
+}
