37404681d51cf9e0257fd0ead6501d64c01fe90b
loki <lion9638@naver.com>
2025-04-14 18:25:13 +0900

    feat: Auth 유즈케이스 작성

--- presentation/src/main/java/com/and/presentation/screen/login/LoginViewModel.kt
+++ presentation/src/main/java/com/and/presentation/screen/login/LoginViewModel.kt
@@ -3,15 +3,14 @@ package com.and.presentation.screen.login
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.and.domain.usecase.user.LoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-
+    private val loginUseCase: LoginUseCase
 ) : ViewModel() {
     private val _loginFailed = mutableStateOf(false)
     val loginFailed: State<Boolean> = _loginFailed
-
-
 }
