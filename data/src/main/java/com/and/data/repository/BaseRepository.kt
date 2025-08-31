317d64b0dec4470b52e00d70994f9025d54c2718
loki <lion9638@naver.com>
2025-08-31 15:01:24 +0900

    refactor | API 요청 리턴 값이 Flow로 래핑된 경우, ApiException을 throw하여 취소되지 않도록 수정

--- data/src/main/java/com/and/data/repository/BaseRepository.kt
+++ data/src/main/java/com/and/data/repository/BaseRepository.kt
@@ -3,10 +3,14 @@ package com.and.data.repository
 import com.and.domain.util.ApiException
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import retrofit2.HttpException
+import java.io.IOException
 import java.lang.Exception
+import kotlin.coroutines.cancellation.CancellationException
 
 abstract class BaseRepository(
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
@@ -31,9 +35,40 @@ abstract class BaseRepository(
                 JSONObject(rawErrorBody).optString("message", e.message())
             }.getOrDefault(e.message())
             throw ApiException(e.code(), msg)
+        }  catch (e: IOException) {
+            if (e.message?.contains("Canceled", ignoreCase = true) == true) {
+                throw CancellationException("OkHttp call canceled", e)
+            }
+            throw ApiException(-1, "네트워크 오류")
+        } catch (e: Exception) {
+            throw ApiException(-1, "알 수 없는 오류")
+        }
+    }
+
+    protected fun <T, R> handleApiFlow(
+        apiCall: suspend () -> T,
+        mapper: (T) -> R
+    ): Flow<R> = flow {
+        try {
+            val dto = withContext(ioDispatcher) { apiCall() }
+            emit(mapper(dto))
+        } catch (e: HttpException) {
+            val rawErrorBody = e.response()?.errorBody()?.string().orEmpty()
+            val msg = runCatching {
+                JSONObject(rawErrorBody).optString("message", e.message())
+            }.getOrDefault(e.message())
+//            throw ApiException(e.code(), msg)
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            if (e.message?.contains("Canceled", ignoreCase = true) == true) {
+                return@flow
+            } else {
+//                throw ApiException(-1, "네트워크 오류")
+            }
         } catch (e: Exception) {
             e.printStackTrace()
-            throw ApiException(-1, "네트워크 오류")
+//            throw ApiException(-1, "알 수 없는 오류")
         }
     }
 }
