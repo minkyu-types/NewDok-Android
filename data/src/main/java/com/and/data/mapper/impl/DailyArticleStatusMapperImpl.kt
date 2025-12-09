8ccf02edce8bded819f27ac99c9881fd71bc18ef
loki <lion9638@naver.com>
2025-12-10 01:09:16 +0900

    chore

---
+++ data/src/main/java/com/and/data/mapper/impl/DailyArticleStatusMapperImpl.kt
@@ -0,0 +1,27 @@
+package com.and.data.mapper.impl
+
+import com.and.data.mapper.DailyArticleStatusMapper
+import com.and.data.model.response.GetArticlesResponseDto.DailyArticleStatusDto
+import com.and.domain.model.DailyArticleStatus
+import javax.inject.Inject
+
+class DailyArticleStatusMapperImpl @Inject constructor(
+): DailyArticleStatusMapper {
+    override fun mapToData(input: DailyArticleStatus): DailyArticleStatusDto {
+        return DailyArticleStatusDto(
+            publishDate = input.publishDate,
+            hasArticles = input.hasArticles,
+            totalCount = input.totalCount,
+            unreadCount = input.unreadCount
+        )
+    }
+
+    override fun mapToDomain(input: DailyArticleStatusDto): DailyArticleStatus {
+        return DailyArticleStatus(
+            publishDate = input.publishDate,
+            hasArticles = input.hasArticles,
+            totalCount = input.totalCount,
+            unreadCount = input.unreadCount
+        )
+    }
+}
